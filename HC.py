14e9e4e2998348a39009cb7fce1621829aa98cc7
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-04-17 14:59:12 +0300

    Update HC.py

--- HC.py
+++ HC.py
@@ -1,3 +1,4 @@
+#====== Задание 1. Калькулятор =============
 # a = int(input("Enter the first number: "))
 # b = int(input("Enter the second number: "))
 # m = input("Enter the operator:")
@@ -13,18 +14,32 @@
 #   print("Invalid operator")
 # # print (f"Произведение: {a*b}, Сумма: {a+b}, Разность: {a-b}, Частное: {a/5}")
 
+#====== Задание 2. Високосный год =============
 # year = int(input("Enter the year: "))
+# if year % 4 != 0:
+#   print(f"Год {year} не является високосным")
+# elif year % 100 == 0:
+#   if year % 400 == 0:
+#     print(f"Год {year} является високосным")
+#   else:
+#     print(f"Год {year} не является високосным")
+# else:
+#   print(f"Год {year} является високосным")
 
-if year % 4 != 0:
-  print(f"Год {year} не является високосным")
-elif year % 100 == 0:
-  if year % 400 == 0:
-    print(f"Год {year} является високосным")
-  else:
-    print(f"Год {year} не является високосным")
-else:
-  print(f"Год {year} является високосным")
+#====== Задание 3. Шахматная доска ============
+# for i in range (4):
+#   print('* * * * ')
+#   print(' * * * *')
 
-num = int(input("Enter the number: "))
-a = num // 100
-print(a)
+#====== Задание 4. Сумма цифр трехзначного числа =============
+# num = int(input("Enter the number: "))
+# while (num != 0):
+#   if num > 99 and num < 1000:
+#     first = num // 100
+#     second = num % 100 // 10
+#     third = num % 10
+#     print(f"Sum of digits: {first + second + third}")
+#   else:
+#     print("Invalid number, try again")
+#   num = int(input("Enter the new number or type 0 for exit: "))
+# print("Ok, bye")
