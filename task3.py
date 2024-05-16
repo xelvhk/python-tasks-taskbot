fb4ffa2551fffb20047dcc7975e80201fe7f4145
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-16 11:33:57 +0000

    All tasks added to tasksnetology.py

--- task3.py
+++
@@ -1,8 +0,0 @@
-def count_letter (words,letter):
-  count=0
-  for word in words:
-    if letter in word:
-      count+=1
-  return count
-  
-print(count_letter(['python', 'c++', 'c', 'scala', 'java'],'c'))
