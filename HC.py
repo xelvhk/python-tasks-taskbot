5da310364c3727d62d9c9e1fc3d063a91797dc8f
xelvhk <166012830+xelvhk@users.noreply.github.com>
2024-05-17 22:03:20 +0300

    Update HC.py

--- HC.py
+++ HC.py
@@ -1,30 +1,3 @@
-#====== Task 1. Simple calculator =============
-# a = int(input("Enter the first number: "))
-# b = int(input("Enter the second number: "))
-# m = input("Enter the operator:")
-# if m == "+":
-#   print(f"Sum: {a+b}")
-# elif m == "-":
-#   print(f"Difference: {a-b}")
-# elif m == "*":
-#   print(f"Product: {a*b}")
-# elif m == "/":
-#   print(f"Quotient: {a/b}")
-# else:
-#   print("Invalid operator")
-
-#====== Task 2. Leap year =============
-# year = int(input("Enter the year: "))
-# if year % 4 != 0:
-#   print(f"Year {year} is not leap")
-# elif year % 100 == 0:
-#   if year % 400 == 0:
-#     print(f"Year {year} is leap")
-#   else:
-#     print(f"Year {year} is not leap")
-# else:
-#   print(f"Year {year} is leap")
-
 #====== Task 3. Chessboard. V.1 ============
 # for i in range (4):
 #   print('* * * * ')
