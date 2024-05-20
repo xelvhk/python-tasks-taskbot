fbe205563715cf873fb806273b5a570b6079c987
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-20 10:12:50 +0300

    Create 5 - Training schedule.py

---
+++ 5 - Training schedule.py
@@ -0,0 +1,8 @@
+#========= Task 5. Training schedule ==========
+# dist = 10
+# days = 0
+# while (dist < 25):
+#   dist = dist + dist * 0.1
+#   days += 1
+#   print(f'{days} days, {dist} km')
+# print(f"The athlete needs {days} days")
