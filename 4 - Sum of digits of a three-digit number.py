4052c632e5e23783f49bb4cd4513ee4e48aa748d
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-20 10:10:14 +0300

    Create 4 - Sum of digits of a three-digit number.py

---
+++ 4 - Sum of digits of a three-digit number.py
@@ -0,0 +1,12 @@
+#====== Task 4. Sum of digits of a three-digit number =============
+# num = int(input("Enter the number: "))
+# while (num != 0):
+#   if num > 99 and num < 1000:
+#     first = num // 100
+#     second = num % 100 // 10
+#     third = num % 10
+#     print(f"Sum of digits: {first + second + third}")
+#   else:
+#     print("Invalid number, try again")
+#   num = int(input("Enter the new number or type 0 for exit: "))
+# print("Ok, bye")
