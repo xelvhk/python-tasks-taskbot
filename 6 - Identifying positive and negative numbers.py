53c9ba51289d679351d6c7c2339f7e8d42e00d1e
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-21 10:28:54 +0300

    Create 6 - Identifying positive and negative numbers.py

---
+++ 6 - Identifying positive and negative numbers.py
@@ -0,0 +1,9 @@
+#========= Task 6. Identifying positive and negative numbers ==========
+# num = int(input('Enter the number: '))
+# while (num != 0):
+#   if num > 0:
+#     print('number is positive')
+#   elif num < 0:
+#     print('number is negative')
+#   num = int(input('Enter the new number or  type 0 for exit: '))
+# print('It's 0, bye')
