1313ef118fab67f9e591559812bb9de920b57aac
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-22 00:04:31 +0300

    Create 7-8 Multiples of 3.py

---
+++ 7-8 Multiples of 3.py
@@ -0,0 +1,7 @@
+#========= Task 7. Multiples of 3 ==========
+# for i in range(3, 100, 3):
+#   print(i)
+
+#========= Task 8. Multiples of 3. Reverse order ==========
+# for i in range(99, 0, -3):
+#   print(i)
