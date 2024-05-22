b0c2788b0a197cf21725d366af7f2e0793858c7e
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-22 12:50:09 +0300

    Update HC.py

--- HC.py
+++ HC.py
@@ -1,15 +1,3 @@
-#========= Task 9. Adding and removing elements from list with append and remove methods ==========
-# first_arr = []
-# for i in range (10):
-#   if i % 2 == 0:
-#     first_arr.append(i)
-# print(first_arr)
-
-# # for i in range (len(first_arr)):
-# #   if i == len(first_arr) - 1:
-# #     first_arr.remove(first_arr[i])
-# # print(first_arr)
-
 #========= Task 10. Sum of list elements ==========
 # sum = 0
 # for i in range (len(first_arr)):
