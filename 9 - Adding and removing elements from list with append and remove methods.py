4476ddac3385da8443d879566498748c03782e41
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-22 12:50:39 +0300

    Create 9 - Adding and removing elements from list with append and remove methods.py

---
+++ 9 - Adding and removing elements from list with append and remove methods.py
@@ -0,0 +1,11 @@
+#========= Task 9. Adding and removing elements from list with append and remove methods ==========
+# first_arr = []
+# for i in range (10):
+#   if i % 2 == 0:
+#     first_arr.append(i)
+# print(first_arr)
+
+# # for i in range (len(first_arr)):
+# #   if i == len(first_arr) - 1:
+# #     first_arr.remove(first_arr[i])
+# # print(first_arr)
