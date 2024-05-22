cd41166b4092ca00cb796c0d7a688e162bb543e2
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-22 12:51:58 +0300

    Update HC.py

--- HC.py
+++ HC.py
@@ -1,9 +1,3 @@
-#========= Task 10. Sum of list elements ==========
-# sum = 0
-# for i in range (len(first_arr)):
-#   sum+=first_arr[i]
-# print(sum)
-
 #========= Task 11. Case inversion ==========
 # word = "HeLLo"
 # result = ""
