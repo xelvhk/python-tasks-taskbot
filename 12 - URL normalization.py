fe2732765a2d28e275de982b4ed3218d99a04818
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-23 11:36:14 +0300

    Create 12 - URL normalization.py

---
+++ 12 - URL normalization.py
@@ -0,0 +1,10 @@
+#========= Task 12. URL normalization ==========
+# url = input("Введите URL: ")
+# result = ""
+# if "https://" in url:
+#   result = url
+# elif "http://" in url:
+#   result = "https://" + url[7:]
+# else:
+#   result = "https://" + url
+# print(result)
