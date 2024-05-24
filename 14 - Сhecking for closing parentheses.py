56b141fdee930a827c8873c3a28ebf291b570339
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-24 15:08:19 +0300

    Create 14 - Сhecking for closing parentheses.py

---
+++ 14 - Сhecking for closing parentheses.py
@@ -0,0 +1,19 @@
+#========= Task 14. Сhecking for closing parentheses ==============
+# def isBalanced():
+#   balance = 0
+#   text = str(input("Enter the string: "))
+#   for i in range(len(text)):
+#     if text[i] == "(":
+#       balance += 1
+#     elif text[i] == ")":
+#       balance -= 1
+#   print(balance)
+  
+#   if balance < 0:
+#     return "not all parentheses are open"
+#   elif balance == 0:
+#     return "all ok"
+#   else:
+#     return "not all parentheses are closed"
+
+# print(isBalanced())
