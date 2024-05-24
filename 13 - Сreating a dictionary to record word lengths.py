9ca3c1bf0f53d38745a22717b727ea27c0c943fa
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-24 15:07:19 +0300

    Create 13 - Сreating a dictionary to record word lengths.py

---
+++ 13 - Сreating a dictionary to record word lengths.py
@@ -0,0 +1,6 @@
+#========= Task 13. Сreating a dictionary to record word lengths ==========
+# dict = {}
+# for i in range(int(input("Number of words? "))):
+#   word = input("Enter a word: ")
+#   dict[word] = len(word)
+# print(dict)
