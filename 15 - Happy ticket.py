bd1fafce0c612dded9469242e282b2411a3a6049
Lex <166012830+xelvhk@users.noreply.github.com>
2024-05-25 15:25:45 +0300

    Create 15 - Happy ticket.py

---
+++ 15 - Happy ticket.py
@@ -0,0 +1,12 @@
+#========= Task 15. Happy ticket ==============
+# def happy_ticket():
+#   ticket = input("Enter the ticket number:")
+#   left_part = ticket[3:]
+#   right_part = ticket[:3]
+
+#   if sum(map(int, left_part)) == sum(map(int, right_part)):
+#     return "Yeah!"
+#   else:
+#     return "Don't worry. Choose the nesxt one"
+
+# print(happy_ticket())
